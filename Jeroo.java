540103c1debd64796dcb2aec165fddb2179be0d0
henry225 <2250039@lfschools.net>
2020-09-02 21:53:53 +0000

    Planted the cross.

--- Jeroo.java
+++ Jeroo.java
@@ -4,10 +4,112 @@
  */
 public class Jeroo extends JerooBase {
 
+//start of plant cross, calling to all helpermethods.
+ public void plantCross(){
+  moveToTop();
+  plantTopRight();
+  plantBottomRight();
+  plantBottomLeft();
+  plantTopLeft();
+}
+//move to top of the cross, in order to start planting.
+public void moveToTop(){
+  hopSeven();
+  turn(RIGHT);
+  hopThree();
+}
+//hop seven in order to get above the top of the cross
+public void hopSeven(){
+  hop();
+  hop();
+  hop();
+  hop();
+  hop();
+  hop();
+  hop();
+}
+//hop three in order to get right above the top of the cross
+public void hopThree(){
+  hop();
+  hop();
+  hop();
+}
+//start planting the top right quadrant of the cross
+public void plantTopRight(){
+  plantTurnLeft();
+  hop();
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  plantHop();
+  turn(LEFT);
+  plantHop();
+  plantHop();
+  plantHop();
+}
+//start planting the bottom right quadrant of the cross
+public void plantBottomRight(){
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  plantHop();
+  plantTurnLeft();
+  hop();
+  plantHop();
+  plantHop();
+  plant();
+}
+//start planting the bottom left quadrant of the cross
+public void plantBottomLeft(){
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  plant();
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  plantHop();
+  plantTurnLeft();
+  plantHop();
+  plantHop();
+  plantHop();
+  plantTurnRight();
+  plantHop();
+  plantHop();
+  plant();
+}
+//start planting the top left quadrant of the cross
+public void plantTopLeft(){
+  turn(RIGHT);
+  plantHop();
+  plantHop();
+  plantHop();
+  plantTurnLeft();
+  hop();
+  plantHop();
+  plantHop();
+  plantTurnRight();
+  hop();
+}
+//in order to clean up the code, use planthop when there is a plant and then a hop in succession. 
+public void plantHop(){
+  plant();
+  hop();
+}
+//in order to clean up the code, use plantTurnLeft when there is a plant and turn(LEFT) in succession
+public void plantTurnLeft(){
+  plant();
+  turn(LEFT);
+}
+//in order to clean up the code, use plantTurnRight when there is a plant and turn(RIGHT) in succesion
+public void plantTurnRight(){
+  plant();
+  turn(RIGHT);
+}
     
-    public void plantCross() {
-        
-    }
     // Put any helpermethods here
     
     
